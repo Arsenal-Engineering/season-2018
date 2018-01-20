5961acdf902e75fe53e850b736359db48abb017b
Evan Merlock <vnmrlck@gmail.com>
2018-01-19 22:16:39 -0600

    Talon should now allow for sensor checking

--- src/main/kotlin/frc/team6223/utils/srx/TalonMotor.kt
+++ src/main/kotlin/frc/team6223/utils/srx/TalonMotor.kt
@@ -1,14 +1,27 @@
 package frc.team6223.utils.srx
 
 import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.SensorCollection
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 
-class TalonMotor(talonId: Int) {
-    private val talonSrx: TalonSRX = TalonSRX(talonId)
+class TalonMotor(talonId: Int, quadratureEnabled: Boolean) {
+    private val talonSrx: TalonSRX  = TalonSRX(talonId)
     val followers: List<FollowerSRX> = ArrayList()
 
-    val reportedPosition = talonSrx.getSelectedSensorPosition(0)
-    val reportedVelocity = talonSrx.getSelectedSensorVelocity(0)
+    private var sensorCollection: SensorCollection? = null
+
+    // todo: convert to distance
+    val talonPosition: Int
+        get() {
+            return sensorCollection?.pulseWidthPosition ?: 0
+        }
+
+    // todo: convert to velocity
+    val talonVelocity: Int
+        get() {
+            return sensorCollection?.pulseWidthVelocity ?: 0
+        }
 
     fun setPercentOut(percentOut: Double) {
         talonSrx.set(ControlMode.PercentOutput, percentOut)
@@ -19,6 +32,16 @@ class TalonMotor(talonId: Int) {
         private val follower: TalonSRX = TalonSRX(followerId)
         init {
             follower.follow(this@TalonMotor.talonSrx)
+        }
+    }
+
+    init {
+        if (quadratureEnabled) {
+            // we have CTRE mag encoder, swap to relative mode
+            this.talonSrx.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0)
+            // todo: decrease status 3 frame period - see SRM 20.3 and 20.8
+            this.sensorCollection = talonSrx.sensorCollection
+
         }
     }
 
