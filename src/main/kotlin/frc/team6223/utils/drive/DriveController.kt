e718c967cfe4efb10f4ac85389d6938481674427
Evan Merlock <vnmrlck@gmail.com>
2018-01-23 09:02:13 -0600

    Moved all the DriveController classes to one file

--- src/main/kotlin/frc/team6223/utils/drive/DriveController.kt
+++ src/main/kotlin/frc/team6223/utils/drive/DriveController.kt
@@ -1,6 +1,7 @@
 package frc.team6223.utils.drive
 
 import com.ctre.phoenix.motorcontrol.ControlMode
+import com.kauailabs.navx.frc.AHRS
 
 
 data class DriveControllerOutput(val controlMode: ControlMode, val left: Double, val right: Double);
@@ -11,4 +12,18 @@ interface DriveController {
     public fun start();
     public fun stop();
 
+}
+
+class ControllerInput(leftEncoder: Int, leftEncoderRate: Int, rightEncoder: Int, rightEncoderRate: Int,
+                      navX: AHRS) {
+    val leftRotations = leftEncoder
+    val leftRotationRate = leftEncoderRate
+    val rightRotations = rightEncoder
+    val rightRotationRate = rightEncoderRate
+
+    val yawRotation = navX.yaw
+    val yawRate = navX.rate
+
+    val pitchRotation = navX.pitch
+    val rollRotation = navX.roll
 }
