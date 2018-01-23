4d98cff6ed8bea1f88e21a32db15e1d70db035f4
Evan Merlock <vnmrlck@gmail.com>
2018-01-23 13:48:38 -0600

    Added experimental support for differentiating between open- and closed- loop

--- src/main/kotlin/frc/team6223/utils/srx/TalonMotor.kt
+++ src/main/kotlin/frc/team6223/utils/srx/TalonMotor.kt
@@ -25,8 +25,12 @@ class TalonMotor(talonId: Int, quadratureEnabled: Boolean = false) {
             return Velocity.convertMagPulseRateToVelocity(sensorCollection?.quadratureVelocity ?: 0)
         }
 
-    fun setPercentOut(percentOut: Double) {
-        talonSrx.set(ControlMode.PercentOutput, percentOut)
+    fun set(mode: MotorControlMode = MotorControlMode.VoltagePercentOut, percentOut: Double) {
+        when(mode) {
+            MotorControlMode.ExperimentalMotionProfiling ->
+                throw UnsupportedOperationException("Motion Profiling is experimental and uses it's own separate API.")
+            else -> talonSrx.set(ControlMode.PercentOutput, percentOut)
+        }
     }
 
 
@@ -48,5 +52,35 @@ class TalonMotor(talonId: Int, quadratureEnabled: Boolean = false) {
 
         }
     }
+
+}
+
+/**
+ * The supported control modes for all [TalonMotor]'s.
+ *
+ * If a method is labeled as Closed Loop, it _must_ still receive input. The reasoning behind marking something as
+ * closed or open loop is for checking if the motor controller can be finished (closed-loop can be, open-loop cannot).
+ */
+enum class MotorControlMode {
+    /**
+     * Open Loop Voltage Percentage Out
+     */
+    VoltagePercentOut,
+    /**
+     * Closed Loop [frc.team6223.utils.pid.PIDFController] Distance
+     */
+    PIDDistance,
+    /**
+     * Closed Loop [frc.team6223.utils.pid.PIDFController] Velocity
+     */
+    PIDVelocity,
+    /**
+     * Closed Loop [frc.team6223.utils.pid.PIDFController] Turn to Relative Angle
+     */
+    PIDRelativeAngle,
+    /**
+     * Closed Loop Motion Profiling (experimental!)
+     */
+    ExperimentalMotionProfiling,
 
 }
