49e995f65d4799afc3bed15c0afef8c50e861f4b
Tommy Droese <tommyd2500@hotmail.com>
2018-01-24 15:20:18 -0600

    Added Smart Dashboard Abilities to Detect Inverted, Distance, and Velocity to both left and right talon motors.

--- src/main/java/frc/team6223/robot/teleoperated/TeleopUtilities.java
+++ src/main/java/frc/team6223/robot/teleoperated/TeleopUtilities.java
@@ -1,11 +1,18 @@
 package frc.team6223.robot.teleoperated;
 
+
 import frc.team6223.utils.srx.TalonMotor;
 
 public class TeleopUtilities {
 
-    public static void putValuesOnDash(TalonMotor left, TalonMotor right) {
-
+    public static void putValuesOnDash(TalonMotor left, TalonMotor right)
+    {
+        SmartDashboard.putBoolean("Inverted Left", left.getInverted());
+        SmartDashboard.putBoolean("Inverted Right", right.getInverted());
+        SmartDashboard.putString("Distance Left", left.getPosition().toString());
+        SmartDashboard.putString("Distance Right", right.getPosition().toString());
+        SmartDashboard.putString("Velocity Left", left.getVelocity().toString());
+        SmartDashboard.putString("Velocity Right", right.getVelocity().toString());
     }
 
 }
