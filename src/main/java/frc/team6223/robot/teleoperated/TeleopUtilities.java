f6cffa69d0bcc557a5c046482fbd01ce803d8c8c
Tommy Droese <tommyd2500@hotmail.com>
2018-01-24 14:57:15 -0600

    Add teleoperated utilities

---
+++ src/main/java/frc/team6223/robot/teleoperated/TeleopUtilities.java
@@ -0,0 +1,11 @@
+package frc.team6223.robot.teleoperated;
+
+import frc.team6223.utils.srx.TalonMotor;
+
+public class TeleopUtilities {
+
+    public static void putValuesOnDash(TalonMotor left, TalonMotor right) {
+
+    }
+
+}
