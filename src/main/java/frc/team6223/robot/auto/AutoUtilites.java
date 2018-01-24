490df7a3df0a8bbe50dedbf9c5688b9d374c0152
Josh Grant <joshuajamesgrant02@gmail.com>
2018-01-24 15:46:47 -0600

    Created sendable chooser for robot starting position in the auto utilities class

--- src/main/java/frc/team6223/robot/auto/AutoUtilites.java
+++ src/main/java/frc/team6223/robot/auto/AutoUtilites.java
@@ -2,6 +2,10 @@ package frc.team6223.robot.auto;
 
 public class AutoUtilites {
     public static SendableChooser<String> generateSendableChooser() {
-
+        SendableChooser sendableChooser = new SendableChooser<>();
+        sendableChooser.addObject("Right");
+        sendableChooser.addDefault("Center");
+        sendableChooser.addObject("Left");
+        return sendableChooser;
     }
 }
