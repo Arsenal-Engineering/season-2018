76e4f1594151ad5dfa69591c020039a99b5ef21f
Evan Merlock <vnmrlck@gmail.com>
2018-01-24 18:32:21 -0600

    Fixed the autonomous utility method and added the sendable choosers to the SmartDashboard

--- src/main/java/frc/team6223/robot/auto/AutoUtilites.java
+++ src/main/java/frc/team6223/robot/auto/AutoUtilites.java
@@ -1,11 +1,15 @@
 package frc.team6223.robot.auto;
 
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+
 public class AutoUtilites {
     public static SendableChooser<String> generateSendableChooser() {
-        SendableChooser sendableChooser = new SendableChooser<>();
-        sendableChooser.addObject("Right");
-        sendableChooser.addDefault("Center");
-        sendableChooser.addObject("Left");
+        SendableChooser<String> sendableChooser = new SendableChooser<>();
+        sendableChooser.addObject("Right", "Right");
+        sendableChooser.addDefault("Center", "Center");
+        sendableChooser.addObject("Left", "Left");
         return sendableChooser;
     }
 }
