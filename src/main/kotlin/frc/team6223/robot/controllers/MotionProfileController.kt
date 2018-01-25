c4aa04d02b53a1f7b557a10d05ca43569b212545
Evan Merlock <vnmrlck@gmail.com>
2018-01-25 13:54:58 -0600

    Added comments to the MotionProfile controller

--- src/main/kotlin/frc/team6223/robot/controllers/MotionProfileController.kt
+++ src/main/kotlin/frc/team6223/robot/controllers/MotionProfileController.kt
@@ -13,14 +13,40 @@ import jaci.pathfinder.Waypoint
 import jaci.pathfinder.followers.EncoderFollower
 import jaci.pathfinder.modifiers.TankModifier
 
+/**
+ * MotionProfileController uses a [Trajectory] and a maximum [Velocity] to run motors to follow a path pre-calculated
+ * by the user. A [MotionProfileController] should be pre-calculated during the robotInit() function, preferably
+ * after you allocate the subsystems.
+ *
+ * @param trajectory The pre-generated trajectory for the Motion Profile
+ * @param maxVelocity The maximum velocity that the robot can go
+ */
 class MotionProfileController(private val trajectory: Trajectory,
                               private val maxVelocity: Velocity): DriveController {
 
+    /**
+     * The internal left trajectory
+     */
     private lateinit var leftTrajectory: Trajectory
+
+    /**
+     * The internal right trajectory
+     */
     private lateinit var rightTrajectory: Trajectory
+
+    /**
+     * The follower for the left trajectory that ensures that the left side of the robot follows correctly
+     */
     private lateinit var leftTrajectoryFollower: EncoderFollower
+
+    /**
+     * The follower for the right trajectory that ensures that the right side of the robot follows correctly
+     */
     private lateinit var rightTrajectoryFollower: EncoderFollower
 
+    /**
+     * Calculates the motor output based on the provided motion profile
+     */
     override fun calculateMotorOutput(controllerInput: ControllerInput): DriveControllerOutput {
         val leftMotor = leftTrajectoryFollower.calculate(controllerInput.leftRotations.toInt())
         val rightMotor = rightTrajectoryFollower.calculate(controllerInput.rightRotations.toInt())
@@ -34,6 +60,9 @@ class MotionProfileController(private val trajectory: Trajectory,
         return DriveControllerOutput(MotorControlMode.VoltagePercentOut, leftMotor + turn, rightMotor - turn)
     }
 
+    /**
+     * Generate the trajectories for the left and the right
+     */
     override fun start(leftInitial: Distance, rightInitial: Distance) {
         println("Generating trajectory")
         val tankModifier = TankModifier(trajectory).modify(wheelBaseWidth)
@@ -69,6 +98,9 @@ class MotionProfileController(private val trajectory: Trajectory,
         println("Started motion profile controller")
     }
 
+    /**
+     * Determine if the trajectory is complete and report it to the user
+     */
     override fun stop() {
         if (leftTrajectoryFollower.isFinished) {
             println("Finished moving on motion profile")
@@ -77,6 +109,13 @@ class MotionProfileController(private val trajectory: Trajectory,
         }
     }
 
+    /**
+     * A secondary constructor that will accept an array of [Waypoint]s and a trajectory configuration.
+     *
+     * @param points The array of [Waypoint]s that turns into the trajectory
+     * @param trajectoryConfig The configuration of the trajectory
+     * @param maxVelocity The maximum velocity the robot can move at
+     */
     constructor(points: Array<Waypoint>, trajectoryConfig: Trajectory.Config, maxVelocity: Velocity):
             this(Pathfinder.generate(points, trajectoryConfig), maxVelocity)
 }
